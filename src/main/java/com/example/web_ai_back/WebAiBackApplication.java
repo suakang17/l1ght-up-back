f3b239c62f844a6252221e1ed6fdbf5869037b09
Sua Kang <rkdtndk09@gmail.com>
2023-05-13 22:52:40 +0900

    initial commit

---
+++ src/main/java/com/example/web_ai_back/WebAiBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.web_ai_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAiBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebAiBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/web_ai_back/WebAiBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.web_ai_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAiBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
